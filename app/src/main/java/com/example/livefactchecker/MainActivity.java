f4aa596a4c4861fcc4c2d57ffde81676495762dd
busterhymen666 <78467763+busterhymen666@users.noreply.github.com>
2025-09-06 02:00:11 -0500

    Create MainActivity.java

---
+++ app/src/main/java/com/example/livefactchecker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.livefactchecker;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Live Fact Checker Placeholder App");
+        setContentView(tv);
+    }
+}
